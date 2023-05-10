b278428be7e854dabf8dcc019f56d915b94014ad
Naklen <dryagin.ser@yandex.ru>
2023-05-11 00:12:45 +0500

    init

---
+++ lab1_mergeSort/MergeSort.cs
@@ -0,0 +1,61 @@
+﻿namespace aca_lab1_merge_sort;
+
+public class MergeSort
+{
+    public static T[] Sort<T>(T[] source) where T : IComparable
+    {
+        T[] tmp;
+        var currentSrc = source;
+        var currentTarget = new T[source.Length];
+
+        var size = 1;
+        while (size < source.Length)
+        {
+            for ( var i = 0; i < source.Length; i += 2 * size ) 
+                Merge(currentSrc, i, currentSrc, i + size, currentTarget, i, size);
+
+            tmp = currentSrc;
+            currentSrc = currentTarget;
+            currentTarget = tmp;
+
+            size *= 2;
+        }
+        return currentSrc;
+    }
+
+    static void Merge<T>(
+        T[] firstArray, int firstStart,
+        T[] secondArray, int secondStart,
+        T[] target, int targetStart,
+        int size) where T : IComparable
+    {
+        var index1 = firstStart;
+        var index2 = secondStart;
+
+        var firstEnd = Math.Min(firstStart + size, firstArray.Length);
+        var secondEnd = Math.Min(secondStart + size, secondArray.Length);
+
+        if (firstStart + size > firstArray.Length)
+        {
+            for (var i = firstStart; i < firstEnd; i++)
+                target[i] = firstArray[i];
+            return;
+        }
+
+        var iterationCount = firstEnd - firstStart + secondEnd - secondStart;
+
+        for (var i = targetStart; i < targetStart + iterationCount; i++)
+        {
+            if (index1 < firstEnd && (index2 >= secondEnd || firstArray[index1].CompareTo(secondArray[index2]) < 0))
+            {
+                target[i] = firstArray[index1];
+                index1++;
+            }
+            else
+            {
+                target[i] = secondArray[index2];
+                index2++;
+            }
+        }
+    }
+}
---
+++ lab1_mergeSort/Program.cs
@@ -0,0 +1,7 @@
+﻿using aca_lab1_merge_sort;
+
+var arr = new int[] { 1, 5, 3, 85, 3, 8, 1, 44, 8, 3, 744, 8, 56654, 897, 4, 879, 46, 6, 58, 6, 49, 8, 8654, 65, 4984, 6565, 4 };
+
+foreach (var e in MergeSort.Sort(arr))
+    Console.Write($"{e} ");
+
